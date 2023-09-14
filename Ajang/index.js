495b1e5613fc7107cbb4c51e81e9277e423b56b0
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-09-14 19:08:10 +0900

    lec14

--- Ajang/index.js
+++ Ajang/index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
-
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({ extended: true })); //application/x-www-form-urlencoded
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
     res.send("Hello Wor??ld");
 });
 
-app.post("api/register", (req, res) => {
+app.post("/api/register", (req, res) => {
     //회원가입할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
     const user = new User(req.body);
@@ -40,7 +40,7 @@ app.post("api/register", (req, res) => {
         });
 });
 
-app.post("api/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }).then((user) => {
         if (!user) {
@@ -69,6 +69,30 @@ app.post("api/login", (req, res) => {
     });
 });
 
-app.get("/api/users/auth", auth, (req, res) => {});
+app.get("/api/users/auth", auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image,
+    });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+    console.log(req.user._id);
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" })
+        .then(() => {
+            return res.status(200).send({ success: true });
+        })
+        .catch((err) => {
+            console.log(req.user._id);
+            return res.json({ success: false, err });
+        });
+});
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
