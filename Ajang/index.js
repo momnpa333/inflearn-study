8a5bfd81b55fdb9b38caf6ffe075392830960c5b
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-09-14 10:50:34 +0900

    make post

--- Ajang/index.js
+++ Ajang/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true })); //application/x-www-form-urlencoded
+
+app.use(bodyParser.json()); //application/json
+
 const mongoose = require("mongoose");
 mongoose
     .connect(
@@ -8,6 +15,8 @@ mongoose
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            //useCreateIndex: true,
+            //useFindAndModify: false,
         }
     )
     .then(() => console.log("MongoDB Connected..."))
@@ -16,4 +25,24 @@ mongoose
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+app.post("/register", (req, res) => {
+    //회원가입할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+    // user.save((err, userInfo) => {
+    //     if (err) return res.json({ success: false, err }); //json형식으로 에러메세지 전달
+    //     return res.status(200).json({
+    //         success: true,
+    //     });
+    // });
+    user.save()
+        .then(() => {
+            res.status(200).json({ success: true });
+        })
+        .catch((err) => {
+            res.json({ success: false, err });
+        });
+});
+
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
