d45a498793cb46b5b179dc0473beb33a90ddd203
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-09-17 11:30:55 +0900

    lec29

--- Ajang/client/src/components/views/LoginPage/LoginPage.js
+++ Ajang/client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage() {
-    return <div>LoginPage</div>;
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    };
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    };
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log("Email", Email);
+        console.log("Password", Password);
+
+        let body = {
+            email: Email,
+            passwords: Password,
+        };
+
+        Axios.post("api/user/login", body).then(response);
+    };
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                height: "100vh",
+            }}
+        >
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={Password}
+                    onChange={onPasswordHandler}
+                />
+                <br />
+                <button>Login</button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginPage;
