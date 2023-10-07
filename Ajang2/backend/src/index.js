d43e19192871572346b067edace8f2ef7043a82e
Kwon Da Woon <momnpa333@gmail.com>
2023-10-07 17:39:45 +0900

    change registerpage

--- Ajang2/backend/src/index.js
+++ Ajang2/backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
+
 const port = 8080;
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -8,13 +10,18 @@ const mongoose = require("mongoose");
 dotenv.config();
 
 //const HOST = "0.0.0.0";
-app.use(cors());
-app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+    console.log("Hi from client3");
+});
 
 app.get("/", (req, res, next) => {
     setImmediate(() => {
@@ -34,7 +41,6 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.send(error.message || "Internal Server00 Error");
 });
-
 app.use(express.static(path.join(__dirname, "../uploads")));
 
 // app.listen(PORT, HOST);
--- Ajang2/frontend/src/main.jsx
+++ Ajang2/frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store, persistor } from "./store";
+import { store, persistor } from "./store/index.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
