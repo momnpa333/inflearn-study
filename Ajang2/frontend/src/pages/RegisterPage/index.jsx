c9c2654871fd16dea54b866749b10a484f339cdd
Kwon Da Woon <momnpa333@gmail.com>
2023-10-07 18:53:07 +0900

    make validate

--- Ajang2/frontend/src/pages/RegisterPage/index.jsx
+++ Ajang2/frontend/src/pages/RegisterPage/index.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const RegisterPage = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm({ mode: "onChange" });
+
+    const onSubmit = ({ email, password, name }) => {
+        reset();
+    };
+
+    const userEmail = {
+        required: "필수 필드입니다.",
+    };
+    const userName = {
+        required: "필수 필드입니다.",
+    };
+    const userPassword = {
+        required: "필수 필드입니다.",
+        minLength: {
+            value: 6,
+            message: "최소 6자입니다.",
+        },
+    };
+
     return (
         <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
             <div className="p-6 bg-sky-500/20 rounded-md shadow-md">
                 <h1 className="text-3xl font-semibold text-center">회원가입</h1>
-                <form className="mt-6">
+                <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
                     <div className="mb-2">
                         <label
                             htmlFor="email"
@@ -17,7 +43,15 @@ const RegisterPage = () => {
                             type="email"
                             id="email"
                             className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+                            {...register("email", userEmail)}
                         />
+                        {errors?.email && (
+                            <div>
+                                <span className="text-red-500">
+                                    {errors.email.message}
+                                </span>
+                            </div>
+                        )}
                     </div>
                     <div className="mb-2">
                         <label
@@ -30,7 +64,15 @@ const RegisterPage = () => {
                             type="name"
                             id="name"
                             className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+                            {...register("name", userName)}
                         />
+                        {errors?.name && (
+                            <div>
+                                <span className="text-red-500">
+                                    {errors.name.message}
+                                </span>
+                            </div>
+                        )}
                     </div>
                     <div className="mb-2">
                         <label
@@ -38,6 +80,28 @@ const RegisterPage = () => {
                             className="text-sm font-semibold text-gray-800"
                         >
                             Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+                            {...register("password", userPassword)}
+                        />
+                        {errors?.password && (
+                            <div>
+                                <span className="text-red-500">
+                                    {errors.password.message}
+                                </span>
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="mb-2">
+                        <label
+                            htmlFor="password"
+                            className="text-sm font-semibold text-gray-800"
+                        >
+                            Password 확인
                         </label>
                         <input
                             type="password"
