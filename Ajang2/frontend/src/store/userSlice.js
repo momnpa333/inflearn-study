de3c58ba9223c8146270872b49ce672c6713eedf
Kwon Da Woon <momnpa333@gmail.com>
2023-10-09 19:46:43 +0900

    register

--- Ajang2/frontend/src/store/userSlice.js
+++ Ajang2/frontend/src/store/userSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { registerUser } from "./thunkFunctions";
+import { toast } from "react-toastify";
 
 const initialState = {
     userData: {
@@ -21,14 +22,19 @@ const userSlice = createSlice({
     extraReducers: (builder) => {
         builder
             .addCase(registerUser.pending, (state) => {
+                console.log(1);
                 state.isLoading = true;
             })
             .addCase(registerUser.fulfilled, (state) => {
+                console.log(2);
                 state.isLoading = false;
+                toast.info("회원가입 성공함");
             })
             .addCase(registerUser.rejected, (state, action) => {
+                console.log(3);
                 state.isLoading = false;
                 state.error = action.payload;
+                toast.error(action.payload);
             });
     },
 });
